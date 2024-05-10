34f83650db1f2a4b6b0aae8812132dab078a015f
JotiRoy01 <jotiroygit@gmail.com>
2024-05-10 10:27:17 +0600

    app.py edit

--- app.py
+++ app.py
@@ -80,4 +80,10 @@ def index():
 
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001, debug=True)
-	app.run(debug=True)
+
+
+app.run(debug=True)
+
+#joti roy
+
+#i am in the master
